74cc4d0f4f5cb633ea40c0c28c3f1f5dc2801bba
Mohnish2004 <mohnishgopi@gmail.com>
2023-07-11 18:17:11 +0400

    large file delete

--- app/(auth)/Contact/page.tsx
+++ app/(auth)/Contact/page.tsx
@@ -1,16 +1,13 @@
 export const metadata = {
-  title: 'Home - Simple',
+  title: 'Contact - Simple',
   description: 'Page description',
 }
+
 import Testimonials from '@/components/testimonials'
 import Stats from '@/components/stats'
 import Clients from '@/components/clients'
 import Contact_intro from '@/components/contact_intro'
-import Experience_banner_1 from '@/components/experience_banner1'
-import Experience_banner_2 from '@/components/experience_banner2'
-import Experience_banner_3 from '@/components/experience_banner3'
-import Experience from '@/components/Experience'
-import Experience_accordian from '@/components/Experience_accordian'
+
 
 
 export default function Home() {
