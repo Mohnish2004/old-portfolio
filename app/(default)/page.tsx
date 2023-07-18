feea4d0aeccefa4beabc7f5c12f6097b212c17bb
Mohnish2004 <mohnishgopi@gmail.com>
2023-07-18 12:29:08 +0400

    testing for jim

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <Features />
       <Strings2 />
       <FeaturesBlocks />
-      <Clients />
       <Testimonials />
       <Analytics />
     </>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -70,12 +70,12 @@ export default function Example() {
 
           <ol className="relative border-l border-gray-800 dark:border-gray-900 ">
             <li className="mb-10 ml-6 px-4 py-5 duration-150 hover:border-gray-800 hover:rounded-xl hover:bg-gray-800">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-900 rounded-full -left-3 ring-8 ring-gray-900 dark:ring-gray-900 dark:bg-blue-900">
                 <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                 </svg>
               </span>
-              <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">University of Califorina, Davis<span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Latest</span></h3>
+              <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-100 dark:text-white">University of Califorina, Davis<span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Latest</span></h3>
               <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Expected Graduation: June 2026</time>
               <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Bachelor of Science in Computer Science & Engineering Dean's List x2</p>
               <a href="https://docs.google.com/document/d/e/2PACX-1vTx1dG5lHXLgkZOmtqWeIrbph_rc2qQ3Y1T_-d5sWrXNmuKF6kkIOQaUOr5Uww87w/pub" target="_blank" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg className="w-3.5 h-3.5 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={500} height={500} fill="currentColor" viewBox="0 0 20 20">
