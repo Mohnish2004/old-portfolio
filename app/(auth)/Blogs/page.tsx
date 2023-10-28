89d92cc548de80b63821301aaae3a2a4291d45f4
Mohnish2004 <mohnishgopi@gmail.com>
2023-10-28 14:04:36 -0700

    new client

--- app/(auth)/Blogs/page.tsx
+++ app/(auth)/Blogs/page.tsx
@@ -1,73 +1,69 @@
 
-
-export const metadata = {
-    title: 'About me',
-    description: 'This is where you can find information about me',
-  }
+'use client'
   
 import { Analytics } from '@vercel/analytics/react';
-import About_intro from '@/components/about_intro'
-import About_banner from '@/components/about_banner'
-import About_info from '@/components/about_info'
-import About_pics from '@/components/about_pics'
-import Modal from '@/components/modal'
-
+import Spline from '@splinetool/react-spline'; // Replace with the path to your banner image
 
   
   export default function Home() {
     return (
       <>
-      <About_intro/>
-      <About_banner/>
-      <About_info/>
-      <Modal/>
+<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <br></br>
+<Spline scene="https://prod.spline.design/YeA-MY8eEYt2RWxb/scene.splinecode"/>
+  <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+    <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Insights</h2>
+    <p className="mt-1 text-gray-600 dark:text-gray-400">Stay in the know with insights from industry experts.</p>
+  </div>
 
-      <div className=" mx-auto px-5 py-2\3 lg:px-32 lg:py-2 ">
+  <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
-
-      <div className="bg-black py-6 sm:py-8 lg:py-12">
-  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-    <div className="mb-10 md:mb-16">
-    <h1 className="text-gray-200 text-6xl font-semibold sm:text-6xl">
-    <iframe className="border-radius:12px" src="https://open.spotify.com/embed/playlist/0yIGIUWcMpxYiW2Emhibrb?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <br></br>
-      Favourite Films 🎥
-          </h1>
-    </div>
-
-    <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
-
-      <div>
-        <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-          <img src="https://i.etsystatic.com/43778811/r/il/e6c6c1/4982576486/il_570xN.4982576486_7qi7.jpg" loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-
-        </a>
-
-      </div>
-      <div>
-        <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-          <img src="https://i.etsystatic.com/22147411/r/il/5c0a06/3890833949/il_fullxfull.3890833949_aru7.jpg" loading="lazy" alt="Photo by Nick Karvounis" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
+    <a className="group rounded-xl overflow-hidden" href="#">
+      <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+        <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80" alt="Image Description"/>
+        <span className="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-gray-900">
+          Sponsored
+        </span>
       </div>
 
-      <div>
-        <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-          <img src="https://www.spieltimes.com/wp-content/uploads/2022/12/Miles-Morales-Across-The-Spiderverse-scaled.jpg" loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
+      <div className="mt-7">
+        <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+          Studio by Preline
+        </h3>
+        <p className="mt-3 text-gray-800 dark:text-gray-200">
+          Produce professional, reliable streams easily leveraging Preline's innovative broadcast studio
+        </p>
+        <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+          Read more
+          <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </p>
+      </div>
+    </a>
 
+    <a className="group rounded-xl overflow-hidden" href="#">
+      <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+        <img className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl" src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description"/>
       </div>
 
-      <div>
-        <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-          <img src="https://i.ebayimg.com/images/g/ZYkAAOSwbiFZW6oA/s-l1200.webp" loading="lazy" alt="Photo by Vladimir Fedotov" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-        </div>
+      <div className="mt-7">
+        <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+          Onsite
+        </h3>
+        <p className="mt-3 text-gray-800 dark:text-gray-200">
+          Optimize your in-person experience with best-in-class capabilities like badge printing and lead retrieval
+        </p>
+        <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+          Read more
+          <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </p>
       </div>
-    </div>
+    </a>
   </div>
 </div>
-      <About_pics/>
 
       <Analytics />
       </>
